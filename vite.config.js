24a6200c169390487937bcf89646d449f9f1ed9a
lucas-bernardo-souza <lucasbernardodesouza510@gmail.com>
2025-05-14 07:25:25 -0300

    Inicialização do projeto

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        background: resolve(__dirname, 'src/background.js'),
+        content: resolve(__dirname, 'src/content.js'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    },
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  publicDir: 'public', // Garante que manifest.json e ícones vão para dist/
+})
